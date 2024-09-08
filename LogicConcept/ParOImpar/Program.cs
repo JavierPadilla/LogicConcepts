4353ac91a718ff6e221a81a141e7df46b7eab562
JavierPadilla <host_javier@hotmail.com>
2024-09-07 20:02:40 -0600

    ejercicio01

---
+++ LogicConcept/ParOImpar/Program.cs
@@ -0,0 +1,22 @@
+﻿do
+{
+    Console.Write("Introduzca un numero entero o Control + C para salir : ");
+    var n = 0;
+    if (int.TryParse(Console.ReadLine(), out n))
+    {
+        if (n % 2 == 0)
+        {
+        Console.WriteLine("El numero {0} es Par :)", n);
+        }
+         else
+         {
+             Console.WriteLine("El numero {0} es Impar :", n);
+         }
+    
+    }
+    else
+    {
+        Console.WriteLine("Debe ingresar un valor numerico entero ");
+    }
+
+} while (true);
